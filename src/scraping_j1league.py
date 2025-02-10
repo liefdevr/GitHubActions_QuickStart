8b54463804d57df2599aa69fb8f793d3471666d0
liefdevr <71309699+liefdevr@users.noreply.github.com>
2025-02-10 16:47:28 +0900

    Update scraping_j1league.py

--- src/scraping_j1league.py
+++ src/scraping_j1league.py
@@ -5,5 +5,19 @@ response = request.urlopen('https://www.jleague.jp/standings/j1/')
 soup = BeautifulSoup(response)
 response.close()
 
-header = soup.find('tr')
-print(header)
+table = soup.find_all('tr')
+
+standing = []
+for row in table:
+    tmp = []
+    for item in row.find_all('td'):
+        if item.a:
+            tmp.append(item.text[0:len(item.text) // 2])
+        else:
+            tmp.append(item.text)
+    del tmp[0]
+    del tmp[-1]
+    standing.append(tmp)
+
+for item in standing:
+    print(item)
