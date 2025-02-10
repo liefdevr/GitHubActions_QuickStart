32c32fafcd253b9cbad5216277dc2f23d178cb70
liefdevr <71309699+liefdevr@users.noreply.github.com>
2025-02-10 16:26:51 +0900

    Update request_sejuku.py

--- src/request_sejuku.py
+++ src/request_sejuku.py
@@ -8,4 +8,12 @@ response.close()
 
 topstories = soup.find('div', class_='colBoxTopstories')
 colboxindexes = topstories.find_all('div', class_='colBoxIndex')
-print(colboxindexes[0])
+
+top_articles = []
+for item in colboxindexes:
+    title = item.select('div.colBoxTitle')[0].text
+    description = item.select('div.colBoxDescription')[0].text
+    top_articles.append(f'{title}: {description}')
+
+for articles in top_articles:
+    print(articles)
